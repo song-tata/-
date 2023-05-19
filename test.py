c8684d9500f8c34274eb0301203034437e93194e
song_tata <kgsong8020@naver.com>
2023-05-19 09:16:45 +0900

    init: cos pro 코드 연습 파일
    코드 저장용

--- test.py
+++ test.py
@@ -45,4 +45,63 @@ def solution(n,votes):
     votes_len = len(votes)
     candidate = votes[0]
     count = 1
+    range(1,n+1)
+    
+    return answer
+ 
 
+n = 3
+votes = [1,2,3,2,1,3,2,1,3]  
+
+for i in votes:
+  print(i)
+# list 순서 뒤집기
+
+#-----------------------------
+
+# def solution(arr):
+#     left = 0
+#     right = len(arr)-1
+#     # for i in range(len(arr)):
+#     #     idx = arr[left]
+#     #     arr[left] = arr[right]
+#     #     arr[right] = idx
+#     #     left += 1
+#     #     right -= 1
+#     #     if(left>right):
+#     #         break 
+#     while(left<right):
+#         tmp = arr[left]
+#         arr[left] = arr[right]
+#         arr[right] = tmp
+#         left += 1
+#         right -= 1 
+#     return arr
+
+# arr1 = [1,2,3,4]
+# arr2 = [3,5,7,8,2,1]
+
+# print(solution(arr1))
+# print(solution(arr2))
+
+# 징검다리 건너기
+
+# ------------------------
+
+# stones = [1,1,3,2,4,2,1,3,1,2]
+
+# cnt = 0
+# n = len(stones)
+# frog_rocation = 0
+# while(n>frog_rocation):
+#     cnt += 1
+#     frog_rocation += stones[frog_rocation]
+# print(stones)
+# print(str(cnt)+"번 뜀")
+
+
+# import random
+# a = random.randint(1,6)
+
+# arr = [1,2,3,4]
+# arr.pop()
